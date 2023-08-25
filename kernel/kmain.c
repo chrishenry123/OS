e9441b7fdc2be259c6f4a3a06e61a0e6c2e6a53d
dylancaldwell <drc0010@mix.wvu.edu>
2023-08-25 15:53:27 -0400

    Correct implementation of commandhandler, need functions within to be set

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -17,42 +17,92 @@ static void klogv(device dev, const char *msg)
 
 void kmain(void)
 {
-    // Initialize serial I/O first so we have a means of communication
+    // 0) Serial I/O -- <mpx/serial.h>
+    // If we don't initialize the serial port, we have no way of
+    // performing I/O. So we need to do that before anything else so we
+    // can at least get some output on the screen.
+    // Note that here, you should call the function *before* the output
+    // via klogv(), or the message won't print. In all other cases, the
+    // output should come first as it describes what is about to happen.
+
+
+    serial_init(COM1);
+    //serial_out(COM1, buffer, len);
     klogv(COM1, "Initialized serial I/O on COM1 device...");
 
-    // Initialize Global Descriptor Table
+    // 1) Global Descriptor Table (GDT) -- <mpx/gdt.h>
+    // Keeps track of the various memory segments (Code, Data, Stack, etc.)
+    // required by the x86 architecture. This needs to be initialized before
+    // interrupts can be configured.
     klogv(COM1, "Initializing Global Descriptor Table...");
+    gdt_init();
 
-    // Initialize Interrupt Descriptor Table
+    // 2) Interrupt Descriptor Table (IDT) -- <mpx/interrupts.h>
+    // Keeps track of where the various Interrupt Vectors are stored. It
+    // needs to be initialized before Interrupt Service Routines (ISRs) can
+    // be installed.
     klogv(COM1, "Initializing Interrupt Descriptor Table...");
+    idt_init();
 
-    // Disable Interrupts while setting up
+    // 3) Disable Interrupts -- <mpx/interrupts.h>
+    // You'll be modifying how interrupts work, so disable them to avoid
+    // crashing.
     klogv(COM1, "Disabling interrupts...");
+    cli();
 
-    // Initialize Interrupt Request routines
+    // 4) Interrupt Request (IRQ) -- <mpx/interrupts.h>
+    // The x86 architecture requires ISRs for at least the first 32
+    // Interrupt Request (IRQ) lines.
     klogv(COM1, "Initializing Interrupt Request routines...");
+    irq_init();
 
-    // Initialize Programmable Interrupt Controller
+
+    // 5) Programmable Interrupt Controller (PIC) -- <mpx/interrupts.h>
+    // The x86 architecture uses a Programmable Interrupt Controller (PIC)
+    // to map hardware interrupts to software interrupts that the CPU can
+    // then handle via the IDT and its list of ISRs.
     klogv(COM1, "Initializing Programmable Interrupt Controller...");
+    pic_init();
 
-    // Re-enable interrupts now that they're set up
+    // 6) Reenable interrupts -- <mpx/interrupts.h>
+    // Now that interrupt routines are set up, allow interrupts to happen
+    // again.
     klogv(COM1, "Enabling Interrupts...");
+    sti();
 
-    // Initialize Virtual Memory
+    // 7) Virtual Memory (VM) -- <mpx/vm.h>
+    // Virtual Memory (VM) allows the CPU to map logical addresses used by
+    // programs to physical address in RAM. This allows each process to
+    // behave as though it has exclusive access to memory. It also allows,
+    // in more advanced systems, the kernel to swap programs between RAM and
+    // storage (such as a hard drive or SSD), and to set permissions such as
+    // Read, Write, or Execute for pages of memory. VM is managed through
+    // Page Tables, data structures that describe the logical-to-physical
+    // mapping as well as manage permissions and other metadata.
     klogv(COM1, "Initializing Virtual Memory...");
+    vm_init();
 
-    // Module specific initialization
+    // 8) MPX Modules -- *headers vary*
+    // Module specific initialization -- not all modules require this.
     klogv(COM1, "Initializing MPX modules...");
     // R5: sys_set_heap_functions(...);
     // R4: create commhand and idle processes
 
-    // Transfer control to the command handler after all initialization
+    // 9) YOUR command handler -- *create and #include an appropriate .h file*
+    // Pass execution to your command handler so the user can interact with
+    // the system.
     klogv(COM1, "Transferring control to commhand...");
-    comhand();  // At this point, the user can provide input
+    // R4: __asm__ volatile ("int $0x60" :: "a"(IDLE));
+    //__asm__ volatile ("int $0x60" :: "a"(IDLE));
+    comhand();
 
-    // Begin system shutdown procedure (this will only be reached if comhand() returns)
+    // 10) System Shutdown -- *headers to be determined by your design*
+    // After your command handler returns, take care of any clean up that
+    // is necessary.
     klogv(COM1, "Starting system shutdown procedure...");
 
-    // Halt the CPU or perform any final steps
+    // 11) Halt CPU -- *no headers necessary, no changes necessary*
+    // Execution of kmain() will complete and return to where it was called
+    // in boot.s, which will then attempt to power off Qemu or halt the CPU.
     klogv(COM1, "Halting CPU...");
 }
