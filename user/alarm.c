2ed5a88b84c4c90faa61a31a50e564737ceffaff
loud <loud@loud-20.04>
2023-10-27 14:48:44 -0400

    Created the function to recieve time/msg from user to set the alarm

--- user/alarm.c
+++ user/alarm.c
@@ -7,6 +7,7 @@
 #include <sys_req.h>
 #include <string.h>
 #include <time.h>
+#include <stdlib.h>
 
 #define RTC_INDEX_PORT 0x70
 #define RTC_DATA_PORT 0x71
@@ -15,16 +16,76 @@
 #define RTC_HOURS_REG 0x04
 
 
-//void alarm(struct time trigger, char* msg) {
+void alarm(struct time trigger, char* msg) {
 
-//	int current_s, current_m, current_h;
+	int current_s, current_m, current_h;
 	
 	// Read seconds, minutes, and hours from RTC registers
-//	current_s = bcdToDecimal(readRTC(RTC_SECONDS_REG));
-//	current_m = bcdToDecimal(readRTC(RTC_MINUTES_REG));
-//	current_h = bcdToDecimal(readRTC(RTC_HOURS_REG));
+	current_s = bcdToDecimal(readRTC(RTC_SECONDS_REG));
+	current_m = bcdToDecimal(readRTC(RTC_MINUTES_REG));
+	current_h = bcdToDecimal(readRTC(RTC_HOURS_REG));
 	
-//	if(trigger.hours > current_h && trigger.minutes > current_m && trigger.seconds > current_s) {
-//	sys_req(WRITE, COM1, msg, strlen(msg));
-//	}
-//}
+	// If the alarm is past the right time to alert the user
+	if(trigger.hours > current_h && trigger.minutes > current_m && trigger.seconds > current_s) {
+	sys_req(WRITE, COM1, msg, strlen(msg));
+	}
+}
+
+void get_alarm() {
+
+	// Prompt user to enter the time for the alarm to go off
+	char time_msg[] = "Please enter the time to set the alarm for in the form hh:mm:ss\n";
+	sys_req(WRITE, COM1, time_msg, strlen(time_msg));
+	
+	char alarmTime[10] = {0};
+	int nread = sys_req(READ, COM1, alarmTime, sizeof(alarmTime));
+	sys_req(WRITE, COM1, alarmTime, nread);
+	
+	// If the time given by the user is not in the correct format, fail out and return
+	if((alarmTime[2] != ':') || (alarmTime[5] != ':')){
+		char errorMsg[] = "Time given for the alarm to go off is not in the proper format hh:mm:ss";
+		sys_req(WRITE, COM1, errorMsg, strlen(errorMsg));
+		return;
+	}
+	
+	// Uses the atoi function to caputere the specific hours, minutes and seconds into corresponding variables
+	int hours = atoi(&alarmTime[0]);
+	int minutes = atoi(&alarmTime[3]);
+	int seconds = atoi(&alarmTime[6]);
+	
+	// Checks to make sure that the time the user gave is within the range to be valid for an alarm
+	if((hours > 23) || (hours < 0)) {
+		char errorMsg[] = "The hours given is out of the range 0 - 23";
+		sys_req(WRITE, COM1, errorMsg, strlen(errorMsg));
+		return;
+	}
+	else if((minutes > 59) || (minutes < 0)) {
+		char errorMsg[] = "The minutes given is out of the range 0 - 59";
+		sys_req(WRITE, COM1, errorMsg, strlen(errorMsg));
+		return;
+	}
+	else if((seconds > 59) || (seconds < 0)) {
+		char errorMsg[] = "The seconds given is out of the range 0 - 59";
+		sys_req(WRITE, COM1, errorMsg, strlen(errorMsg));
+		return;
+	}
+	
+	// If we get here the time entered is in the correct format and valid so we can take a message
+	char user_msg[] = "Please enter the time to set the alarm for in the form hh:mm:ss\n";
+	sys_req(WRITE, COM1, user_msg, strlen(user_msg));
+	
+	char msg[100] = {0};
+	int inread = sys_req(READ, COM1, msg, sizeof(msg));
+	sys_req(WRITE, COM1, msg, inread);
+	
+	// Create a struct time to store the info for this alarm
+	struct time alarm_time;
+	alarm_time.hours = hours;
+	alarm_time.minutes = minutes;
+	alarm_time.seconds = seconds;
+	
+	alarm(alarm_time, msg);
+}
+
+
+
